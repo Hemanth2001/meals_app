c3a46f9cdb1be31701aa85a0e1dcea0d643dba3d
Hemanth2001 <hhemanth660@gmail.com>
2022-12-05 04:32:48 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/categories.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+     theme: ThemeData(colorScheme: ColorScheme.fromSwatch(primarySwatch:
+     Colors.pink).copyWith(secondary: Colors.amber),canvasColor: Color
+         .fromRGBO(255, 254, 229, 1),
+     fontFamily: 'Raleway',
+     textTheme: ThemeData.light().textTheme.copyWith(
+       bodyText1:TextStyle(
+         color: Color.fromRGBO(20, 51, 51, 1),
+       ),
+        bodyText2: TextStyle(
+         color: Color.fromRGBO(20, 51, 51, 1 ),
+
+     ) ,
+         subtitle1:TextStyle(
+           fontSize: 20,
+             fontFamily: 'RobotoCondense',
+           fontWeight: FontWeight.bold
+         )
+     )),
+      home: CategoriesScreen(),
+    );
+  }
+}
+
+
