fbe07f397ca1e545c6d88f3396177cd18c06ccb3
Hemanth2001 <hhemanth660@gmail.com>
2022-12-23 02:54:43 -0500

    meal

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,16 +1,67 @@
 import 'package:flutter/material.dart';
 
-class FilterScreen extends StatelessWidget {
+class FilterScreen extends StatefulWidget {
   const FilterScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FilterScreen> createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+
+  bool _glutenFree=false;
+  bool _vegertarian=false;
+  bool _vegan=false;
+  bool _lactoseFree = false;
+
+  Widget _buildSwitchListTile(
+       String title,
+      String description,
+      bool currentValue,
+      Function updateValue,
+      )
+  {
+    return SwitchListTile(
+        title: Text(title),
+        value: currentValue,
+        subtitle: Text(description),
+
+        onChanged:updateValue(),
+    );
+        }
+
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Filter"
-        ),
+      appBar: AppBar(
+
+        title: Text("Filter")
       ),
+      body: Column(
+        children: [
+          Container(
+            padding:EdgeInsets.all(8.0),
+            child: Text(
+              "Adjust your meal select",
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+          ),
+          Expanded(child: ListView(
+            children: [
+              _buildSwitchListTile('Gluten-free Meals',
+                  'Only include gluten free meals ',_glutenFree,  (newValue){
+                    setState(() {
+                      _glutenFree=newValue;
+                    });
+                  })
+
+            ],
+          ))
+        ],
+      )
     );
   }
 }
