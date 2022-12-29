59e162d4c9e52c6c341475c27bb5bd78b5d928d7
Hemanth2001 <hhemanth660@gmail.com>
2022-12-29 10:53:00 -0500

    meal

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -9,58 +9,52 @@ class FilterScreen extends StatefulWidget {
 
 class _FilterScreenState extends State<FilterScreen> {
 
-  bool _glutenFree=false;
-  bool _vegertarian=false;
-  bool _vegan=false;
+  bool _glutenFree = false;
+  bool _vegertarian = false;
+  bool _vegan = false;
   bool _lactoseFree = false;
-
-
 
   //some issue while passing the function
 
   Widget _buildSwitchListTile(
-       String title,
-      String description,
-      bool currentValue,
-      Function updateValue,
-      )
-  {
+    String title,
+    String description,
+    bool currentValue,
+    Function updateValue,
+  ) {
     return SwitchListTile(
-        title: Text(title),
-        value: currentValue,
-        subtitle: Text(description),
-        onChanged:updateValue(),
+      title: Text(title),
+      value: currentValue,
+      subtitle: Text(description),
+      onChanged: updateValue(),
     );
-        }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-        title: Text("Filter")
-      ),
-      body: Column(
-        children: [
-          Container(
-            padding:EdgeInsets.all(8.0),
-            child: Text(
-              "Adjust your meal select",
-              style: Theme.of(context).textTheme.subtitle1,
+        appBar: AppBar(title: Text("Filter")),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "Adjust your meal select",
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
             ),
-          ),
-          Expanded(child: ListView(
-            children: [
-              _buildSwitchListTile('Gluten-free Meals',
-                  'Only include gluten free meals ',_glutenFree,  (newValue){
-                    setState(() {
-                      _glutenFree=newValue;
-                    });
-                  })
-            ],
-          ))
-        ],
-      )
-    );
+            Expanded(
+                child: ListView(
+              children: [
+                _buildSwitchListTile('Gluten-free Meals',
+                    'Only include gluten free meals ', _glutenFree, (newValue) {
+                  setState(() {
+                    _glutenFree = newValue;
+                  });
+                })
+              ],
+            ))
+          ],
+        ));
   }
 }
